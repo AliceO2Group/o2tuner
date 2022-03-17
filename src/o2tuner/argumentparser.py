c15270709016b5fd5877ebb1336cb6d5af223d77
Matteo Concas <matteo.concas@cern.ch>
2022-03-17 14:17:45 +0100

    Test argParsing and add run mode

--- src/o2tuner/argumentparser.py
+++ src/o2tuner/argumentparser.py
@@ -6,7 +6,6 @@ Module to process command line arguments
 import os.path
 import argparse
 from collections import namedtuple
-from click import option
 import yaml
 
 O2TunerArg = namedtuple("O2TunerArg", "option config descr")
@@ -22,35 +21,32 @@ class O2TunerArgumentParser(argparse.ArgumentParser):
     def __init__(self):
         self.args_normal = []
         super().__init__(formatter_class=argparse.RawTextHelpFormatter)
-
-    def add_argument(self, *args, **kwargs):
-        config_var = None
-        if kwargs.get("config", False):
-            config_var = kwargs["dest"]
-            kwargs["help"] = kwargs.get("help", "") + " [" + config_var + "]"
-
-        self.args_normal.append(O2TunerArg(
-            args[0], config_var, kwargs.get("help", "")))
-        return self.add_argument(*args, **kwargs)
+        super().add_argument("-v", "--version", dest="version", default=False, action="store_true",
+                             help="Print current alidock version on stdout")
+        super().add_argument("-q", "--quiet", dest="quiet", default=False,
+                             action="store_true", help="Do not print any message")
+        super().add_argument("-d", "--debug", dest="debug", default=None,
+                             action="store_true", help="Increase verbosity")
+        super().add_argument("action", default="run",
+                             nargs="?", choices=["run"], help="Run optimiser")
 
     def gen_config_help(self, default_conf):
-        conf_file = os.path.join(
-            os.path.expanduser("~"), ".o2tuner-config.yaml")  # make it cwd
-        epilog = f"it is possible to specify the most frequently used options in a YAML " \
-                 f"configuration file in {conf_file}\n" \
-                 f"the following options (along with their default values) can be specified " \
-                 f"(please include `---` as first line):\n---\n".format(conf_file=conf_file)
+        conf_file = os.path.join(os.getcwd(), ".o2tuner-config.yaml")
+        epilog = f"It is possible to specify the most frequently used options in a YAML " \
+                 f"configuration file in your working directory\n" \
+                 f"Current expected path is: {conf_file}\n" \
+                 f"The following options (along with their default values) can be specified " \
+                 f"(please include `---` as first line):\n---\n"
         yaml_lines = {}
         longest = 0
         for opt in self.args_normal:
             if opt.config:
-                assert opt.config in default_conf, f"option {option} expected in default conf".format(
-                    option=opt.config)
+                assert opt.config in default_conf, f"option {opt.config} expected in default conf"
                 optd = {opt.config: default_conf[opt.config]}
                 yaml_lines[opt.option] = yaml.dump(
                     optd, default_flow_style=False).rstrip()
                 longest = max(longest, len(yaml_lines[opt.option]))
-        fmt = f"%%-{longest}s  # same as option %%s\n".format(longest=longest)
+        fmt = f"%%-{longest}s  # same as option %%s\n"
         for y_line in yaml_lines.items():
             epilog += fmt % (yaml_lines[y_line], y_line)
 

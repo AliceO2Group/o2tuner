3409de1b1c88d1b350bd59bc336850e95702be27
Matteo Concas <matteo.concas@cern.ch>
2022-03-17 11:05:42 +0100

    Add Logging and ArgParsing utils

--- src/o2tuner/__init__.py
+++ src/o2tuner/__init__.py
@@ -3,27 +3,20 @@ o2tuner module
 """
 
 
-from o2tuner.o2tuner import O2tuner
+from o2tuner.o2tuner import O2Tuner
 from o2tuner.backends import OptunaHandler
 
 
 def obj(trial):
-    """
-    This is the objective function to minimise
-    """
     x_var = trial.suggest_float("x", -10, 10)
     return (x_var-2)**2
 
 
 def entrypoint():
-    """
-    Entrypoint of the program
-    """
-
     optuna_handler = OptunaHandler()
     optuna_handler.set_objective(obj)
 
     # Create and run the tuner
-    tuner = O2tuner(optuna_handler)
+    tuner = O2Tuner(optuna_handler)
     tuner.init(n_trials=50)
     tuner.run()
--- src/o2tuner/backends.py
+++ src/o2tuner/backends.py
@@ -20,20 +20,11 @@ class OptunaHandler(object):
         self._n_trials = 100
 
     def initialise(self, n_trials=100):
-        """
-        Initalisation in Optuna involves the creation of study object
-        """
         self._n_trials = n_trials
         self._study = optuna.create_study()
 
     def optimise(self):
-        """
-        Perform study optimisation
-        """
         self._study.optimize(self._objective, n_trials=self._n_trials)
 
     def set_objective(self, objective):
-        """
-        Setup the objective function to minimise/maximise
-        """
         self._objective = objective
